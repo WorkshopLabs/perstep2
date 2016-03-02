998878ecaddd0807256d5a4f741c834dd539e045
AndreaFrancis <soriajimenezandrea@hotmail.com>
2016-03-02 17:43:03 -0400

    File storage completed

--- server/server.js
+++ server/server.js
@@ -2,18 +2,30 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var multer  = require('multer');
-var upload = multer({ dest: './uploads/' })
 
 var port = process.env.PORT || 3030;
+
+/*Storage configuration*/
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+var upload = multer({ storage: storage })
+
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/documents',upload.any(), function (req, res) {
-	console.log("File was saved");
+	console.log("Saving file...");
 });
 
 app.listen( port, function(){
-	console.log('conectado en ', port);
+	console.log('Listen to port: ', port);
 });
 
 
