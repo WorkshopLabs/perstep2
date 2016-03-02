5062e7ae8e0ba8063d56239dc0c380dd512505bb
AndreaFrancis <soriajimenezandrea@hotmail.com>
2016-03-02 18:41:46 -0400

    Minor changes

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ var Firebase = require('firebase');
 var Papa = require('papaparse');
 var ReactFire = require ('reactfire');
 var request = require('superagent');
+
 var firebaseUrl = "https://contactslistfromdoc.firebaseio.com/";
 
 
@@ -15,18 +16,28 @@ var UploadFileBox = React.createClass({
 	componentWillMount: function() {
     	this.bindAsArray(new Firebase(firebaseUrl + "documents"), "registers");
   	},
+  	saveContacts: function(results){
+  		var register =  {fileName: this.state.file.name, contacts: results.data};
+		this.firebaseRefs["registers"].push(register);
+    	request.post("/documents")    
+        	.attach("image-file", this.state.file, this.state.file.name)
+            	.end(function(err, res){
+     					if (err || !res.ok) {
+      					 console.log('Oh no! error');
+     					} else {
+      					 console.log('yay got ' + JSON.stringify(res.body));
+    					}
+    				});
+  	},
+  	askUserBeforeAdding: function(results){
+  		if (confirm('Do you want to add these '+results.data.length+' contacts?')) {
+				this.saveContacts(results);
+				alert("Your register was saved successfully");
+		}
+  	},
 	askUserForFields: function(results) {
         if (confirm('Are these fields correct?: '+results.meta.fields.toString())) {
-    		if (confirm('Do you want to add these '+results.data.length+' contacts?')) {
-				var register =  {fileName: this.state.file.name, contacts: results.data};
-				this.firebaseRefs["registers"].push(register);
-    				 request.post("/documents")    
-            			.attach("image-file", this.state.file, this.state.file.name)
-            			.end(function(res){
-               				 console.log(res);
-            			});
-				alert("Your register was saved successfully");
-			}
+    		this.askUserBeforeAdding(results);
 		}
 	},
 	handleVerify: function(){
@@ -42,11 +53,21 @@ var UploadFileBox = React.createClass({
 	render: function(){
 		return (
 			<div className='uploadFileBox'>
-					<span className='btn btn-default btn-file'>
-    					Browse 
-    					<input type='file'  onChange={this.handleFileChange} accept='.csv'/>
-					</span>
-					<button type='button' className='btn btn-primary' onClick={this.handleVerify}>Next</button>
+				<div className="jumbotron container span12">
+  					<h1>Contacts App</h1>
+  					<p>Choose your file to save your contacts</p>		
+					
+  						<div className="form-group">
+
+   							<span className='btn btn-default btn-file'>
+    							Choose a file 
+    							<input type='file'   onChange={this.handleFileChange} accept='.csv'/>
+							</span>
+  						</div>
+  						<br/>
+  						<button type="button" className="btn btn-primary" onClick={this.handleVerify}>Ok</button>
+
+				</div>
 			</div>
 			);
 	}
